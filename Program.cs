47f0c74113c67cc25d51348d8bd6a4fa0d76b6df
Alyarah <alyarahmadayanis>
2025-03-20 11:17:39 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+using System.Runtime.InteropServices;
+
+public class SimpleDataBase <T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public void SimpleDataBae()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime);
+    }
+
+    public void PrintAllData()
+    {
+        
+    }
+
+    class program
+    {
+        
+    }
+}
